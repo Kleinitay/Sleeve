afd1aa3acf748ca30e2bb3a1b2ac87105e5dba06
Natan Yellin <aantny@gmail.com>
2011-11-17 18:47:32 +0200

    updated annotations

--- Sleeve/app/models/space_file.rb
+++ Sleeve/app/models/space_file.rb
@@ -2,12 +2,16 @@
 #
 # Table name: space_files
 #
-#  id         :integer(4)      not null, primary key
-#  name       :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#  Lat        :integer(10)
-#  Long       :integer(10)
+#  id                  :integer(4)      not null, primary key
+#  name                :string(255)
+#  created_at          :datetime
+#  updated_at          :datetime
+#  Lat                 :integer(10)
+#  Long                :integer(10)
+#  source_file_name    :string(255)
+#  source_content_type :string(255)
+#  source_file_size    :integer(4)
+#  source_updated_at   :datetime
 #
 
 class SpaceFile < ActiveRecord::Base
